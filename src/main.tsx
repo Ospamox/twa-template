fd61465af060ff39c59ca8581199bb6737f6018b
Ospamox <71867223+Ospamox@users.noreply.github.com>
2024-06-14 22:31:30 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,7 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-<<<<<<< HEAD
- 
-=======
->>>>>>> 49d413241e0b0e002f5084df2cf952df4401d03c
+
   "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
