c9517419594b661e373e596403b7aa3d45b95d8c
Ospamox <sheva800@rambler.com>
2024-06-14 15:49:25 +0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -56,3 +56,28 @@ export default App;
 // yarn remove namemodul -удалить модуль
 // yarn upgrade namemodul@<версия> - обновить модуль
 
+
+
+
+// dependencies": {
+//   "@aws-crypto/sha256-js": "^5.0.0",
+//   "@orbs-network/ton-access": "^2.3.3",
+//   "@tanstack/react-query": "^4.24.4",
+//   "@ton/core": "^0.56.3",
+//   "@tonconnect/ui-react": "^1.0.0-beta.6",
+//   "@twa-dev/sdk": "^7.0.0",
+//   "buffer": "^6.0.3",
+//   "gh-pages": "^6.1.1",
+//   "is-mobile": "^3.1.1",
+//   "react": "^18.2.0",
+//   "react-dom": "^18.2.0",
+//   "styled-components": "6.1.11",
+//   "ton": "^13.5.0",
+//   "ton-core": "^0.49.1",
+//   "ton-crypto": "^3.2.0",
+//   "use-local-storage": "^2.3.6",
+//   "vite-plugin-node-polyfills": "^0.7.0"
+// }
+
+// "@twa-dev/sdk": "^7.0.0",
+// "gh-pages": "^6.1.1",
