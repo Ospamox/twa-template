37f2f6dfcb707c5ffeb2f4bfdce9d6f6279c21b0
unknown <sheva800@rambler.com>
2026-06-11 09:59:26 +0300

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from "./App";
 import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 // this manifest is used temporarily for development purposes
-const manifestUrl =
-<<<<<<< HEAD
- 
-=======
->>>>>>> 49d413241e0b0e002f5084df2cf952df4401d03c
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
-const queryClient = new QueryClient({
+const manifestUrl =
+
+  
+  // "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  
+    "https://ospamox.github.io/twa-template/tonconnect-manifest.json";
+    
+
+ const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
+  
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
